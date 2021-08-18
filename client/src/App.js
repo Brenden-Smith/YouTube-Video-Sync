61a32c8b6ddc7955ba80db421f0ed6f3e2b43d99
Brenden Smith <61305154+Brenden-Smith@users.noreply.github.com>
2021-08-18 16:39:13 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,20 @@
 // Components
 import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
 import {
   BrowserRouter as Router,
-  Switch,
   Route,
-  Redirect,
 } from "react-router-dom";
 import PrivateRoute from './components/PrivateRoute';
-
-// Firebase
-import firebase from "./firebase"
 
 // Routes
 import Login from "./routes/Login";
 import Room from "./routes/Room";
 
-
-const auth = firebase.auth();
-
 const App = () => {
-
-  const [user] = useAuthState(auth);
 
   return (
     <Router>
-      {/* <PrivateRoute path="/" exact component={Room}/> */}
-      <Route path="/" exact component={Room}/>
+      <PrivateRoute path="/" exact component={Room}/>
       <Route path="/login" component={Login}/>
     </Router>
   )
