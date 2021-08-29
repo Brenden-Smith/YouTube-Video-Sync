d63ec1ab0a1c8582093ee4a77d31788c5bf6c7e9
Brenden Smith <61305154+Brenden-Smith@users.noreply.github.com>
2021-08-28 18:20:58 -0700

    Create index.js

---
+++ client/src/components/index.js
@@ -0,0 +1,8 @@
+import CommentFeed from "./CommentFeed";
+import NavBar from "./NavBar";
+import PrivateRoute from "./PrivateRoute";
+import QueueFeed from "./QueueFeed";
+import VideoDetails from "./VideoDetails";
+import VideoPlayer from "./VideoPlayer";
+
+export { CommentFeed, NavBar, PrivateRoute, QueueFeed, VideoDetails, VideoPlayer }
