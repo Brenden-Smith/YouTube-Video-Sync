e16c22633597d095872e0d6083df4d92e23af448
Brenden Smith <61305154+Brenden-Smith@users.noreply.github.com>
2021-08-28 18:19:27 -0700

    Combine App.js and Wrapper.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,46 @@
 // Components
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import {
-  BrowserRouter as Router,
-  Route,
-} from "react-router-dom";
-import PrivateRoute from './components/PrivateRoute';
+  createTheme,
+  responsiveFontSizes,
+  useMediaQuery,
+} from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { PrivateRoute } from "./components";
 
 // Routes
 import Login from "./routes/Login";
 import Room from "./routes/Room";
 
 const App = () => {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const theme = responsiveFontSizes(
+    React.useMemo(
+      () =>
+        createTheme({
+          palette: {
+            type: prefersDarkMode ? "dark" : "light",
+            background: {
+              appbar: prefersDarkMode ? "#303030" : "rgba(0, 0, 0, 0.26)",
+              avatar: prefersDarkMode ? "#757575" : "rgba(0, 0, 0, 0.26)",
+              default: prefersDarkMode ? "#424242" : "#fafafa",
+              paper: prefersDarkMode ? "#303030" : "rgba(0, 0, 0, 0.26)",
+            },
+          },
+        }),
+      [prefersDarkMode]
+    )
+  );
 
   return (
-    <Router>
-      <PrivateRoute path="/" exact component={Room}/>
-      <Route path="/login" component={Login}/>
-    </Router>
-  )
-}
+    <ThemeProvider theme={theme}>
+      <Router>
+        <PrivateRoute path="/" exact component={Room} />
+        <Route path="/login" component={Login} />
+      </Router>
+    </ThemeProvider>
+  );
+};
 
 export default App;
