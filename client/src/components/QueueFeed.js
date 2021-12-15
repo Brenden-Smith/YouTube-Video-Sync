f24af8d84e044146cb71b960167a0c87d68c41df
Brenden Smith <61305154+Brenden-Smith@users.noreply.github.com>
2021-12-14 20:08:31 -0800

    Update QueueFeed.js
    - Adjust padding
    - Scrollable

--- client/src/components/QueueFeed.js
+++ client/src/components/QueueFeed.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Box, Container, Divider, Grid, makeStyles, Typography } from "@material-ui/core";
+import { Box, Container, Divider, Grid, List, makeStyles, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
     thumbnail: {
@@ -71,16 +71,16 @@ function QueueCard(props) {
     const classes = useStyles();
 
     return (
-        <Grid container direction="row" spacing={2} style={{width: '100%', padding: 10}}>
-            <Grid item xs={1} align="left">
+        <Grid container direction="row" spacing={2} style={{width: '100%', padding: 10}} wrap="nowrap">
+            <Grid item align="left">
                 <Typography variant="h6"><Box color="text.primary">{props.index+1}.</Box></Typography>
             </Grid>
-            <Grid item xs={3} align="left">
+            <Grid item align="left">
                 <Container className={classes.thumbnail}>
                     {props.video.thumbnail===null ? null : <img src={props.video.thumbnail} style={{height: '100%', width: '100%'}} alt="thumbnail"/>}
                 </Container>
             </Grid>
-            <Grid item xs={8} align="left">
+            <Grid item align="left">
                 <Grid container direction="column">
                     <Grid item xs={12} alignItems="center">
                         <Typography variant="subtitle2"><Box color="text.primary" fontWeight="bold">{props.video.name}</Box></Typography>
@@ -97,16 +97,16 @@ export default class QueueFeed extends Component {
 
     render() {
         return (
-            <div style={{height: '100%', width: '100%', overflow: 'auto', paddingRight: '20px'}}>
-                {Object.keys(TestVideos).map(function(key, index) {
-                    return (
-                        <div>
-                            <QueueCard index={index} video={TestVideos[key]}/>
-                            <Divider/>
-                        </div>
-                    )
-                })}
-            </div>
-        )
+            <List>
+              {Object.keys(TestVideos).map(function (key, index) {
+                return (
+                  <>
+                    <QueueCard index={index} video={TestVideos[key]} />
+                    <Divider />
+                  </>
+                );
+              })}
+            </List>
+        );
     }
 }
