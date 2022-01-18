6d980cbc23c155a614fd9aa088aeaff4622e5eb1
Brenden Smith <61305154+Brenden-Smith@users.noreply.github.com>
2022-01-18 04:37:54 -0800

    Update VideoPlayer.tsx
    Integrate with YouTube Iframe API

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -1,11 +1,16 @@
-import React from "react"
+import { getAuth } from "firebase/auth"
+import { getDatabase, off, onValue, ref, set } from "firebase/database"
+import React, { useEffect, useState } from "react"
+import YouTube, { Options } from "react-youtube"
+import { YouTubePlayer } from "youtube-player/dist/types"
+import "../index.css"
 
 const styles = {
   container: {
     position: "relative",
     overflow: "hidden",
     width: "100%",
-    paddingTop: "56.25%"
+    // paddingTop: "56.25%"
   },
   iframe: {
     position: "absolute",
@@ -17,16 +22,99 @@ const styles = {
 } as any
 
 export default function VideoPlayer(props: any) {
+
+  const [player, setPlayer] = useState<YouTubePlayer>()
+  const [videoID, setVideoID] = useState("")
+  const { room, host } = props
+
+  useEffect(() => {
+
+    const videoQuery = ref(getDatabase(), `rooms/${room}/video/source`)
+    onValue(
+      videoQuery,
+      (snapshot) => {
+        setVideoID(snapshot.val())
+      }
+    )
+    
+    const timeQuery = ref(getDatabase(), `rooms/${room}/video/time`)
+    onValue(
+      timeQuery,
+      (snapshot) => {
+        if (player) {
+          console.log(snapshot.val())
+          if (player.getCurrentTime() > snapshot.val() + 0.12 || player.getCurrentTime() < snapshot.val() - 0.12) {
+            player.seekTo(snapshot.val(), true)
+          }
+        }
+      }
+    )
+
+    const actionQuery = ref(getDatabase(), `rooms/${room}/video/action`) 
+    onValue(actionQuery, (snapshot) => {
+        if (player) {
+          if (snapshot.val() === "play") {
+            player.playVideo()
+          } else if (snapshot.val() === "pause") {
+            player.pauseVideo()
+          }
+        }
+      })  
+
+    return () => {
+      off(actionQuery, "value")
+      off(timeQuery, "value")
+      off(videoQuery, "value")
+    }
+  }, [player, room])
+
+  let timeCount: any;
+
+  let opts: Options = {
+    playerVars: {
+      start: 0,
+      autoplay: 1,
+      enablejsapi: 1,
+      modestbranding: 1,
+    }
+  }
+
+  async function onStateChange(e: { target: YouTubePlayer, data: number }) {
+    switch (e.data) {
+      case YouTube.PlayerState.PLAYING:
+        console.log("Playing")
+        set(ref(getDatabase(), `rooms/${room}/video/action`), "play")
+        if (host === getAuth().currentUser?.uid) {
+          timeCount = setInterval(() => {
+            set(ref(getDatabase(), `rooms/${room}/video/time`), e.target.getCurrentTime())
+          }, 5000)
+        }
+        break
+      case YouTube.PlayerState.PAUSED:
+        set(ref(getDatabase(), `rooms/${room}/video/action`), "pause")
+        if (host === getAuth().currentUser?.uid) {
+          clearInterval(timeCount)
+        }
+        break
+      case YouTube.PlayerState.ENDED:
+        break
+    }
+  }
+
   return (
     <div style={styles.container}>
-      <iframe
-        src={props.src}
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-        title="Embedded youtube"
-        style={styles.iframe}
-      />
+        <YouTube
+          id="player"
+          containerClassName="youtubeContainer"
+          videoId="BO1EYEDdFPo"
+          onEnd={() => { }} // TODO: Call cloud function to update video
+          onStateChange={(e) => onStateChange(e)}
+          onReady={(e) => {
+            console.log("Ready")
+            setPlayer(e.target)
+          }}
+          opts={opts}
+        />
     </div>
   );
 }
