6a5888b74ab4c1062f864e1ef4e7269747acfc63
Brenden Smith <61305154+Brenden-Smith@users.noreply.github.com>
2022-01-18 04:37:40 -0800

    Update index.js
    Set host to be first user who joins -- handle room deletion

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 
@@ -15,7 +16,16 @@ exports.addUser = functions.https.onCall(async (data, context) => {
         displayName: data.displayName,
         photoURL: data.photoURL,
       })
-      .then(() => {
+      .then(async () => {
+        await admin
+            .database()
+            .ref(`rooms/${data.room}/users`)
+            .get().then(async (snapshot) => {
+              if (snapshot.numChildren() === 1) {
+                await admin.database()
+                    .ref(`rooms/${data.room}/host`).set(data.uid);
+              }
+            });
         return 201;
       })
       .catch((error) => {
@@ -28,11 +38,21 @@ exports.addUser = functions.https.onCall(async (data, context) => {
  * Removes a user from the room
  */
 exports.removeUser = functions.https.onCall(async (data, context) => {
+  console.log(data);
   return await admin
       .database()
       .ref(`rooms/${data.room}/users/${data.uid}`)
       .remove()
-      .then(() => {
+      .then(async () => {
+        // Remove the room if there are no users
+        await admin
+            .database()
+            .ref(`rooms/${data.room}/users`)
+            .get().then(async (doc) => {
+              if (doc.val() === null) {
+                await admin.database().ref(`rooms/${data.room}`).remove();
+              }
+            });
         return 201;
       })
       .catch((error) => {
