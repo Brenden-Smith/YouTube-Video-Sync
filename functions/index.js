a76d0d72ec35319a8f5691f808dd8634a8f35857
Brenden Smith <61305154+Brenden-Smith@users.noreply.github.com>
2022-01-19 01:27:29 -0800

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,9 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable max-len */
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const {google} = require("googleapis");
+const service = google.youtube("v3");
 
 admin.initializeApp();
 
@@ -15,6 +18,7 @@ exports.addUser = functions.https.onCall(async (data, context) => {
       .set({
         displayName: data.displayName,
         photoURL: data.photoURL,
+        uid: data.uid,
       })
       .then(async () => {
         await admin
@@ -59,4 +63,44 @@ exports.removeUser = functions.https.onCall(async (data, context) => {
         console.log(error);
         return 500;
       });
+});
+
+/**
+ * Fetch a video from the YouTube API based on search query
+ */
+exports.fetchVideo = functions.https.onCall(async (data, context) => {
+  return await service.videos.list({
+    auth: process.env.FIREBASE_API_KEY,
+    part: "snippet",
+    id: data.query,
+  }).then((response) => {
+    if (response) {
+      return response.data.items[0];
+    } else {
+      return 500;
+    }
+  }).catch((error) => {
+    console.log(error);
+    return 500;
+  });
+});
+
+/**
+ * Fetch creator photo from the YouTube API based on search query
+ */
+exports.fetchChannelThumbnail = functions.https.onCall(async (data, context) => {
+  return await service.channels.list({
+    auth: process.env.FIREBASE_API_KEY,
+    part: "snippet",
+    id: data.query,
+  }).then((response) => {
+    if (response) {
+      return response.data.items[0];
+    } else {
+      return 500;
+    }
+  }).catch((error) => {
+    console.log(error);
+    return 500;
+  });
 });
