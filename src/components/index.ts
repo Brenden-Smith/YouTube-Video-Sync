6e9290bcb72ea7975952cbf0e808799ac1199c56
Brenden Smith <61305154+Brenden-Smith@users.noreply.github.com>
2022-01-19 01:27:17 -0800

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,7 +3,6 @@ import PrivateRoute from "./PrivateRoute";
 import QueueFeed from "./QueueFeed";
 import VideoDetails from "./VideoDetails";
 import VideoPlayer from "./VideoPlayer";
-import RemoteControl from "./RemoteControl";
 import Card from "./Card";
 
-export { Card, NavBar, PrivateRoute, QueueFeed, VideoDetails, VideoPlayer, RemoteControl }
+export { Card, NavBar, PrivateRoute, QueueFeed, VideoDetails, VideoPlayer }
