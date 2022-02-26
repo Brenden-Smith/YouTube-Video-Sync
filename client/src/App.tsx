9d43d6b4122c697186e874840146b97172de8178
Brenden Smith <61305154+Brenden-Smith@users.noreply.github.com>
2022-02-25 16:19:44 -0800

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 // Components
-import { useEffect, useState } from "react";
+import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Route, Routes } from "react-router";
 import { ThemeProvider } from "@mui/material/styles";
@@ -9,42 +9,23 @@ import CreateAppTheme from "./assets/theme";
 // Routes
 import { Home, Login, Room } from "./routes";
 import { CssBaseline } from "@mui/material";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./firebase/firebase";
 
 export default function App() {
-  const [mounted, setMounted] = useState(false);
-  const [authenticated, setAuthenticated] = useState(false);
   const theme = CreateAppTheme();
 
-  // Determine current authentication state
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      setAuthenticated(!!user);
-    });
-    setMounted(true);
-  }, []);
-
   return (
-    <>
-      {mounted && (
-        <ThemeProvider theme={theme}>
-          <CssBaseline>
-            <Router>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route
-                  path="/room/:id"
-                  element={<PrivateRoute auth={authenticated} />}
-                >
-                  <Route path="/room/:id" element={<Room />} />
-                </Route>
-                <Route path="/login" element={<Login />} />
-              </Routes>
-            </Router>
-          </CssBaseline>
-        </ThemeProvider>
-      )}
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/room/:id" element={<PrivateRoute />}>
+              <Route path="/room/:id" element={<Room />} />
+            </Route>
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Router>
+      </CssBaseline>
+    </ThemeProvider>
   );
 }
